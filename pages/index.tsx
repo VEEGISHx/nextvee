c9b8019ed5d069b7979f7eb165860ef1da3ad40b
Veegish Ramdani <veegish@cyberstorm.mu>
2020-11-02 22:29:11 +0400

    Added Navigation component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import Navigation from '../components/Navigation/src/Navigation';
 
 const { CONTENT_API_KEY, BLOG_URL } = process.env;
 
 type Post = {
   title: string;
   slug: string;
+}
+
+type Page = {
+  title: string;
+  slug: string;
+}
+
+interface HomepageProps {
+  posts: Post[]
+  pages: Page[];
 }
 
 async function getPosts() {
@@ -19,20 +29,31 @@ async function getPosts() {
   return posts;
 }
 
+async function getPages() {
+  const results = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/pages/?key=${CONTENT_API_KEY}`
+  ).then((results) => results.json());
+
+  const pages = results.pages;
+
+  return pages;
+}
+
 export const getStaticProps = async ({ params }) => {
   const posts = await getPosts();
+  const pages = await getPages();
+
   return {
-    props: { posts }
+    props: { posts, pages }
   }
 }
 
-const Home:React.FC<{ posts: Post[] }> = (props) => {
-  const { posts } = props;
-  
-  console.log(posts);
+const Home:React.FC<HomepageProps> = (props) => {
+  const { posts, pages } = props;
   
   return (
-    <div className={styles.container}>
+    <div>
+      <Navigation pages={ pages }/>
       <h1>Hello to my blog</h1>
       <ul>
         {posts.map((post, index) => {
